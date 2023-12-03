7402a1d49f2767bef731eca8a79e44d79f09e1cd
Thushara Abeykoon <thushara.abeykoon99@gmail.com>
2023-12-03 15:35:11 +0530

    added background video to user components also

--- front-end web/src/AppUser.js
+++ front-end web/src/AppUser.js
@@ -7,24 +7,36 @@ import { Route, Routes } from "react-router-dom";
 import Home from "./user-components/Home";
 import Account from "./user-components/Account";
 import Create from "./user-components/FontCreator";
+import backgroundVideo from "./videos/background.mp4";
 
 export default function AppUser() {
   return (
-    <div>
-      <HeaderUser />
-      <div
-        className="flex mt-20 justify-center items-center"
-        // style={{ height: "600px" }}
+    <>
+      <video
+        muted
+        autoplay={"autoplay"}
+        preload="auto"
+        loop
+        className="fixed min-h-full min-w-full -z-10"
       >
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/create" element={<Create />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/downloads" element={<Downloads />} />
-          <Route path="/account" element={<Account />} />
-          {/* <Route path="/logout" element={<Register />} /> */}
-        </Routes>
+        <source src={backgroundVideo} type="video/mp4" />
+      </video>
+      <div>
+        <HeaderUser />
+        <div
+          className="flex mt-20 justify-center items-center"
+          // style={{ height: "600px" }}
+        >
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/create" element={<Create />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/downloads" element={<Downloads />} />
+            <Route path="/account" element={<Account />} />
+            {/* <Route path="/logout" element={<Register />} /> */}
+          </Routes>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
