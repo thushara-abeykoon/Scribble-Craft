146e0a0e9ed4e6ec002924829320c0ea22770600
Thushara Abeykoon <thushara.abeykoon99@gmail.com>
2023-12-07 11:35:43 +0530

    added token create for user login

--- back-end/app.py
+++ back-end/app.py
@@ -1,8 +1,17 @@
 from flask import Flask
+from flask_jwt_extended import JWTManager
+
 from user import user
+import secrets
 
 app = Flask(__name__)
 app.register_blueprint(user, url_prefix='/user')
+
+secret_key = secrets.token_urlsafe(32)
+
+app.config['JWT_SECRET_KEY'] = secret_key
+
+jwt = JWTManager(app)
 
 
 @app.route('/')
--- back-end/user.py
+++ back-end/user.py
@@ -1,6 +1,9 @@
 from flask import Blueprint, request, jsonify
+from flask_jwt_extended import create_access_token
+
 from db.db_connector import database_initialize, get_connection
 import mysql.connector
+import hashlib
 
 user = Blueprint('user', __name__)
 
@@ -41,3 +44,22 @@ def login():
     required_fields = ['email', 'password']
     if not parameters_check(data, required_fields):
         return jsonify({"error": "missing parameters"})
+
+    email = data['email']
+    password = data['password']
+
+    hashed_user_password = hashlib.md5(password.encode('utf-8')).hexdigest()
+
+    # check if the password is correct for the particular email
+    conn = get_connection()
+    cursor = conn.cursor()
+    query = f"SELECT password FROM user WHERE email = '{email}'"
+    cursor.execute(query)
+    password_from_db = cursor.fetchone()
+    if cursor.rowcount == 0:
+        return jsonify({"error": "email isn't recognized"}), 401
+    elif hashed_user_password == list(password_from_db)[0]:
+        access_token = create_access_token(identity=email)
+        return jsonify({"access_token": access_token}), 200
+    else:
+        return jsonify({"error": "password isn't valid"}), 403
