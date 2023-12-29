e47d672e1ac7937f2302d54f2a44aeb0620f929a
Thushara Abeykoon <thushara.abeykoon99@gmail.com>
2023-12-30 01:36:57 +0530

    added font generator end poin

--- back-end/font_generator.py
+++ back-end/font_generator.py
@@ -1,23 +1,45 @@
 import os.path
-
+from flask_jwt_extended import jwt_required, get_jwt_identity
 from flask import Blueprint, request, jsonify
 
 font = Blueprint('font', __name__)
 
+if not os.path.exists('uploads'):
+    os.mkdir('uploads')
+
+available_files = []
+
+# for ascii_num in range(48, 90):
+#     if 57 < ascii_num < 65:
+#         continue
+for ascii_num in range(49, 51):
+    available_files.append(chr(ascii_num))
+
 
 @font.route('/manual_generate', methods=['POST'])
+@jwt_required()
 def generate_font():
-    if 'file' not in request.files:
-        return jsonify({'error': 'No file part'}), 400
+    current_user_email = get_jwt_identity()
+    if not os.path.exists('uploads/'+current_user_email):
+        os.mkdir('uploads/' + current_user_email)
 
-    file = request.files['file']
+    if not isAllFilesAvailable(request.files):
+        return jsonify({'error': 'Missing images'}), 400
 
-    if file.filename == '':
-        return jsonify({'error': 'No selected File'}), 400
-
-    if not os.path.exists('uploads'):
-        os.mkdir('uploads')
-
-    file.save('uploads/' + file.filename)
+    for available_file in available_files:
+        file = request.files[available_file]
+        file.save('uploads/'+current_user_email+'/'+available_file+'.'+file.filename.split('.')[-1])
 
     return jsonify({'success': True}), 200
+
+
+def isAllFilesAvailable(files_array):
+    if len(files_array) == 0:
+        return False
+    for available_char in available_files:
+        try:
+            files_array[available_char]
+        except Exception as e:
+            return False
+
+    return True
