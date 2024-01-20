7263d9ca09518f7ba000467bf84f457e2688f017
Thushara Abeykoon <thushara.abeykoon99@gmail.com>
2024-01-20 13:00:55 +0530

    created method to get maximum width of svg glyph

--- back-end/font_config/glyph_manager.py
+++ back-end/font_config/glyph_manager.py
@@ -1,5 +1,17 @@
+import svgpathtools
+
+
 def glyph_creator(glyph_name, unicode, data, horizontal_space):
     if type(data) is not list:
         return f'<glyph glyph-name="{glyph_name}" unicode="{unicode}" d="{data}" horiz-adv-x="{horizontal_space}" />'
     else:
         return f'<glyph glyph-name="{glyph_name}" unicode="{unicode}" horiz-adv-x="{horizontal_space}>{list(map(lambda x: "<path d=" + x + "/>", data))}</glyph>'
+
+
+def get_max_width(path):
+    path = svgpathtools.parse_path(path)
+    bounding_boxes = [subpath.bbox() for subpath in path]
+
+    _, max_width, _, _ = max(box for box in bounding_boxes)
+
+    return max_width
