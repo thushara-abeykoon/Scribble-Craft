45f1b15e0b096ca78e2328791095c919e9e74848
Thushara Abeykoon <thushara.abeykoon99@gmail.com>
2024-01-21 08:34:31 +0530

    created necessary codes for automatic font configuration

--- back-end/app.py
+++ back-end/app.py
@@ -1,13 +1,15 @@
 from flask import Flask
 from flask_jwt_extended import JWTManager
 
+from automatic_config.request_handler import automatic
 from user import user
-from manual_config.request_handler import font
+from manual_config.request_handler import manual
 import secrets
 
 app = Flask(__name__)
 app.register_blueprint(user, url_prefix='/user')
-app.register_blueprint(font, url_prefix='/font')
+app.register_blueprint(manual, url_prefix='/manual')
+app.register_blueprint(automatic, url_prefix='/automatic')
 
 secret_key = secrets.token_urlsafe(32)
 
