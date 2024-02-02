a1c7a3c3b2fc8a125e3a263aa17342e8ba57f908
Thushara Abeykoon <thushara.abeykoon99@gmail.com>
2024-02-03 03:17:56 +0530

    changed font styles

--- front-end web/src/components/LoginRegister.js
+++ front-end web/src/components/LoginRegister.js
@@ -1,12 +1,21 @@
+import axios from "axios";
 import React, { useState } from "react";
 
 export function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("")
+
+  const handleLogin = async(e) => { 
+      e.preventDefault()
+      await axios.post("http://localhost:5000/user/login",{email, password})
+   }
+
   return (
     <div className="box font-mono">
       <p className="boxHeader">Login</p>
-      <form>
-        <input type="email" placeholder="Email" className="inputField" />
-        <input type="password" placeholder="Password" className="inputField" />
+      <form onSubmit={handleLogin}>
+        <input type="email" value={email} onChange={(e)=>setEmail(e.target.value)} placeholder="Email" className="inputField" />
+        <input type="password" value={password} onChange={e=>setPassword(e.target.value)} placeholder="Password" className="inputField" />
         <input type="submit" value="Login" className="button" />
         <p className="text-xs text-stone-200 mt-8 hover:underline hover:underline-offset-2 cursor-pointer">
           Forgot Password?
