fbd3df11a1c75f93909e3581242111800443c2d7
Thushara Abeykoon <thushara.abeykoon99@gmail.com>
2024-02-04 16:10:55 +0530

    added jwt verification for font manipulate

--- back-end/manual_config/request_handler.py
+++ back-end/manual_config/request_handler.py
@@ -1,7 +1,7 @@
 import base64
 import os
 
-from flask import Blueprint, request, jsonify, send_file
+from flask import Blueprint, request, jsonify
 from flask_jwt_extended import jwt_required, get_jwt_identity
 
 from manual_config.font_handler import FontConfig
@@ -12,9 +12,9 @@ font_config = FontConfig()
 
 
 @manual.route('/generate', methods=['POST'])
-# @jwt_required()
+@jwt_required()
 def manual_generate():
-    status = font_config.get_upload(current_user_email="thushara", files_list=request.files)
+    status = font_config.get_upload(current_user_email=get_jwt_identity(), files_list=request.files)
     print(font_config.image_status)
     return status
 
@@ -36,10 +36,9 @@ def create_font():
 
 
 @manual.route('/request-font', methods=['GET'])
-# @jwt_required()
+@jwt_required()
 def get_font():
-    user_email = "thushara"
-    user_folder = os.path.join("users", user_email)
+    user_folder = os.path.join("users", get_jwt_identity())
     if os.path.exists(user_folder):
         print("user folder found")
         font_folder = os.path.join(user_folder, "manual")
