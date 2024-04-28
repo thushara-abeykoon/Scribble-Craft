4deed03ca86de6f150d0a2638c2b0bbe3554f392
Thushara Abeykoon <thushara.abeykoon99@gmail.com>
2024-04-28 13:46:35 +0530

    added new method of background removing instead of rembg package

--- back-end/manual_config/image_config.py
+++ back-end/manual_config/image_config.py
@@ -1,6 +1,7 @@
 import base64
 import json
 import cv2
+from PIL import Image
 import rembg
 import requests
 
@@ -14,15 +15,16 @@ api_key = get_from_txt()
 convertio_url = "https://api.convertio.co/convert"
 
 
-def enhance_image(image):
+def enhance_image(image, thresh_value):
     cv2.resize(image, (200, 200))
     gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    _, threshold_image = cv2.threshold(gray_image, 127, 255, cv2.THRESH_BINARY)
+    _, threshold_image = cv2.threshold(gray_image, thresh_value, 255, cv2.THRESH_BINARY)
 
     return threshold_image
 
 
 def request_svg(image_path, api_index):
+    print(api_key[api_index])
     payload_for_id = {
         "apikey": api_key[api_index],
         "input": "base64",
@@ -32,8 +34,12 @@ def request_svg(image_path, api_index):
     }
 
     res = requests.post(convertio_url, json=payload_for_id)
+    print(res.text)
     if res.status_code != 200:
-        api_index = api_index + 1 if (api_index < len(api_key)) else 0
+        if api_index < len(api_key):
+            api_index = api_index + 1
+        else:
+            api_index = 0
         return request_svg(image_path, api_index)
     return json.loads(res.text)
 
@@ -51,4 +57,22 @@ def convert_image_to_base64_string(filepath):
 
 
 def remove_background(image):
-    return rembg.remove(image)
+    # return rembg.remove(image)
+    # convert opencv image to PIL image
+    color_converted = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    pil_image = Image.fromarray(color_converted)
+
+    pil_image = pil_image.convert('RGBA')
+    datas = pil_image.getdata()
+
+    new_data = []
+    for item in datas:
+        if item[0] == 255 and item[1] == 255 and item[2] == 255:
+            new_data.append((255, 255, 255, 0))
+        else:
+            new_data.append(item)
+
+    pil_image.putdata(new_data)
+    return pil_image
+
+
