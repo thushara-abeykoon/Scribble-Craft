5b470b9c5b4c5d2f1d5dff9ab436dc33d81af792
Thushara Abeykoon <thushara.abeykoon99@gmail.com>
2024-04-28 13:51:02 +0530

    created new endpoint to generate font in automatic way

--- back-end/automatic_config/request_handler.py
+++ back-end/automatic_config/request_handler.py
@@ -2,6 +2,7 @@ from flask import Blueprint, request, jsonify
 from flask_jwt_extended import jwt_required, get_jwt_identity
 
 from automatic_config.font_handler import AutomaticFontConfig
+from model_config.model_handler import ModelConfig
 
 automatic = Blueprint('auto', __name__)
 
@@ -11,7 +12,11 @@ font_config = AutomaticFontConfig()
 @automatic.route('/upload_file', methods=['POST'])
 # @jwt_required
 def upload_file():
-    current_user_email = "thushara"
+    # model_config = ModelConfig("users/thushara2@gmail.com/automatic/characters.jpg")
+    # pred = model_config.get_predictions()
+    # print(pred)
+    # return "hello"
+    current_user_email = "thushara2@gmail.com"
     # current_user_email = get_jwt_identity()
     files = request.files
     print(files)
@@ -23,3 +28,17 @@ def upload_file():
 def request_predictions(character_name):
     character_array = font_config.get_images(character_name)
     return jsonify(character_array), 200
+
+
+@automatic.route('/generate', methods=['POST'])
+# @jwt_required()
+def auto_generate():
+    current_user_email = "thushara2@gmail.com"
+    character_data = request.json
+
+    if type(character_data) is not list:
+        return jsonify({'error': 'Invalid data'}), 400
+
+    return font_config.handle_upload_data(character_data)
+
+
